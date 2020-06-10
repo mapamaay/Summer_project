577709511e445239b3fa928edd553c82a516d063
Romain Guillome <r.guillome@student.isartdigital.com>
2020-06-10 13:45:23 +0200

    Commit Model + Animations Robot
    Commit Robot_Model + Animations Robot V1 + Animation Beam V1 + Script Controller animations

---
+++ Mouvements Test/Assets/PrefabsRomain/Script/AimPosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimPosition : MonoBehaviour
+{
+  	private GameObject beamPosition;
+    public string object_name;
+    Vector3 m_NewPosition;
+	public float m_YPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_NewPosition = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        beamPosition = GameObject.Find(object_name);
+        m_NewPosition = beamPosition.transform.position;
+        m_NewPosition.y = m_YPosition;
+        transform.position = m_NewPosition;
+    }
+}
---
+++ Mouvements Test/Assets/PrefabsRomain/Script/ChangePosition.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePosition : MonoBehaviour
+{
+    private GameObject beam;
+    public string object_name;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        beam = GameObject.Find(object_name);
+        transform.position = beam.transform.position;
+    }
+}
